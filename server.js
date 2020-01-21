747d6ad1fb694d70b6d5b70704aae8608864d6c2
Jon Carlo Vargas <jcjv94@gmail.com>
2020-01-21 10:00:45 -0800

    log in working

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
 
 // Put API routes here, before the "catch all" route
-
+app.use('/api/users', require('./routes/api/users'))
 
 // The following "catch all" route (note the *)is necessary
 // for a SPA's client-side routing to properly work
