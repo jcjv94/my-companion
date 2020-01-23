71a57edef021b7fd2d3404b5aa507652bd68834c
Jon Carlo Vargas <jcjv94@gmail.com>
2020-01-23 14:25:10 -0800

    check boxes complete

--- src/pages/dogDetailPage/dogDetailPage.jsx
+++ src/pages/dogDetailPage/dogDetailPage.jsx
@@ -4,19 +4,45 @@ import { Link } from "react-router-dom";
 
 class dogDetailPage extends Component {
   render() {
+    const walks = Array(this.props.dog.numWalks)
+      .fill()
+      .map((_, idx) => {
+        return (
+          <td class="center-align">
+            <label>
+              <input type="checkbox" name={`walk${idx}`} />
+              <span></span>
+            </label>
+          </td>
+        );
+      });
 
-    const walks = Array(this.props.dog.numWalks).fill().map((_, idx) => {
-      return (
-        <td class="center-align">
-          <label>
-            <input type="checkbox" name={`walk${idx}`}/>
-            <span></span>
-          </label>
-        </td>
-      )
-    })
+    const feedings = Array(this.props.dog.numFeedings)
+      .fill()
+      .map((_, idx) => {
+        return (
+          <td class="center-align">
+            <label>
+              <input type="checkbox" name={`feedings${idx}`} />
+              <span></span>
+            </label>
+          </td>
+        );
+      });
 
-    
+    const meds = Array(this.props.dog.numMeds)
+      .fill()
+      .map((_, idx) => {
+        return (
+          <td class="center-align">
+            <label>
+              <input type="checkbox" name={`meds${idx}`} />
+              <span></span>
+            </label>
+          </td>
+        );
+      });
+
     return (
       <>
         <table>
@@ -47,70 +73,24 @@ class dogDetailPage extends Component {
 
           <tr>
             <th class="">Walks: {this.props.dog.numWalks}</th>
-            {
-              walks.map(walk => walk)
-            }
-            
-                <input type="checkbox" />
-                
+            {walks.map(walk => walk)}
 
+            {/* <input type="checkbox" /> */}
           </tr>
 
           <tr>
             <th class="">Feedings: {this.props.dog.numFeedings}</th>
-            <td class="center-align">
-              <label>
-                <input type="checkbox" />
-                <span></span>
-              </label>
-            </td>
+            {feedings.map(feedings => feedings)}
+
+            {/* <input type="checkbox" /> */}
           </tr>
 
           <tr>
             <th class="">Medications: {this.props.dog.numMeds}</th>
-            <td class="center-align">
-              <label>
-                <input type="checkbox" />
-                <span></span>
-              </label>
-              
-            </td>
-          </tr>
+            {meds.map(meds => meds)}
 
-          {/* <tr>
-            <td class="center-align">
-              <label>
-                <input type="checkbox" />
-                <span></span>
-              </label>
-            </td>
-            <td class="center-align">
-            <label>
-                <input type="checkbox" />
-                <span></span>
-              </label>
-            </td>
-            <td class="center-align">
-            <label>
-                <input type="checkbox" />
-                <span></span>
-              </label>
-            </td>
-            <td class="center-align">
-              <Link
-                to={{
-                  pathname: "/edit",
-                  state: { dog: this.props.dog }
-                }}
-                class="btn btn-small purple"
-              >
-                <i class="material-icons">mode_edit</i>
-              </Link>
-            </td>
-            <td class="center-align">
-              <button onClick={() => this.props.handleDeleteDog(this.props.dog._id)} class="btn btn-small red"><i class="material-icons">delete_forever</i></button>
-            </td>
-          </tr> */}
+            {/* <input type="checkbox" /> */}
+          </tr>
         </table>
         <br />
         <Notes dog={this.props.dog} />
